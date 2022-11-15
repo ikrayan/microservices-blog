a89d20bfd753181065f23ed5ac05af58850aa3a7
ikrayan <82193882+ikrayan@users.noreply.github.com>
2022-11-15 11:38:48 +0700

    dggfdf

--- posts/index.js
+++ posts/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const { randomBytes } = require("crypto");
 
 const app = express();
+app.use(bodyParser.json());
 
 const posts = {};
 
@@ -9,7 +11,18 @@ app.get("/posts", (req, res) => {
   res.send(posts);
 });
 
-app.post("/posts", (req, res) => {});
+app.post("/posts", (req, res) => {
+  const id = randomBytes(4).toString("hex");
+  // oiuasydf8s7fdy78
+  const { title } = req.body;
+
+  posts[id] = {
+    id,
+    title,
+  };
+
+  res.status(201).send(posts[id]);
+});
 
 app.listen(4000, () => {
   console.log("Server listening on port 4000!");
